65db9c21c958b70e852f557f92deb180cf6071cb
Simran Soin <simransoin@Simrans-MacBook-Pro.local>
2017-07-11 16:52:02 -0400

    Add comments throughout app.py

--- app.py
+++ app.py
@@ -13,77 +13,67 @@ wrong_three=""
 
 app = Flask(__name__)
 app.secret_key = secure.APP_SECRET_KEY
+
+#function to make the list_of_words and list_of_definitions based on the session["current_list"]
 def make_lists(list_of_words, list_of_definitions):
+    #name_of_collection is a variable based on the session[current_list]
     name_of_collection = str(session["current_list"]).lower()
     for item in db[name_of_collection].find():
         list_of_words.append(item["word"])
         list_of_definitions.append(item["definition"])     
     return list_of_words, list_of_definitions 
 
-    correct_word= list_of_words[0]
-    correct_definition= list_of_definitions[0]
-    not_the_same = False
-    while not not_the_same:
-        wrong_one = random.choice(list_of_definitions)
-        if correct_definition != wrong_one:
-            not_the_same = True
-        else:
-            continue
-    not_the_same = False
-    while not not_the_same:
-        wrong_two = random.choice(list_of_definitions)
-        if correct_definition != wrong_two:
-            if wrong_one != wrong_two:
-                not_the_same = True
-            else:
-                continue
-        else:
-            continue
-    not_the_same = False
-    while not not_the_same:
-        wrong_three = random.choice(list_of_definitions)
-        if correct_definition != wrong_three:
-            if wrong_one != wrong_three:
-                if wrong_two != wrong_three:
-                    not_the_same = True
-                else:
-                    continue
-            else:
-                continue
-        else:
-            continue
-    list_of_options = [correct_definition, wrong_one, wrong_two, wrong_three]
-    random.shuffle(list_of_options)
-    
           
 @app.route("/",methods=["GET"])
 def main():
     if request.method == "GET":
+        #initially directs user to the homepage which asks the user what they would like to do (quiz, etc). Redirects accordingly
         return render_template("homepage.html" )     
  
 @app.route("/setsession", methods=["GET", "POST"])
 def set_session():
     global list_of_words, list_of_definitions
     if request.method =="GET":
+        #directs user to a page to set the session["current_list"]
         return render_template("set_session.html")
     else:
+        #POST request for when user clicks "submit"
+        #set_session page has a dropdown menu in which the user selects what list he/she would like to cover
+        #the session["current_list"] is set accordingly
         session["current_list"] = request.form.get("current_list")
         list_of_words = []
         list_of_definitions = []
         make_lists(list_of_words, list_of_definitions)
+        #makes list_of_words and list_of_definitions according to the list the user has selected
+        #these lists allow for easier access and editing based on user progress
         return redirect("/quiz", 303)
         
-@app.route("/quiz", methods=["GET","POST"])
+@app.route("/quiz", methods=["GET"])
 def quiz():
+    #user is directed here to actually be asked questions
     global correct_definition, list_of_words, list_of_definitions, correct_word, wrong_one, wrong_two, wrong_three, word_index
-    print(correct_definition)
+    #there are 2 possible scenarios for why there is a GET request being made on this app route
+    #scenario one is that the user has already answered the question and needs to get feedback
+    #scenario two is that the user is accessing the page for the first time
+    # 
+    #if the user response == correct_definition, user got the question right
+    #this word can now be removed, so the user is not asked it again
+    #directs the user to a page saying "CORRECT
     if request.form.get("options") == correct_definition:
         list_of_words.pop(word_index)
-        return "CORRECT"          
+        list_of_definitions.pop(word_index)
+        return "CORRECT"        
+    # 
+    #if there is no user response, the user is simply accessing the page 
     elif request.form.get("options") == None:
+        #a word_index is generated to make the word choice random but also ensure that it corresponds to the same item in list_of_words and list_of_definitions
         word_index= random.randint(0, len(list_of_words)-1)
+        #the correct_word is set
         correct_word= list_of_words[word_index]
+        #the correct_definiton is set
         correct_definition= list_of_definitions[word_index]
+        # a variable called not_the_same is created
+        #a loop is made based on this variable to ensure that wrong_one (one of the incorrect responses) is not the same as correct_definition
         not_the_same = False
         while not not_the_same:
             wrong_one = random.choice(list_of_definitions)
@@ -91,6 +81,7 @@ def quiz():
                 not_the_same = True
             else:
                 continue
+        #this loop also ensures that wrong_two is not the same as wrong_one or correct_definition
         not_the_same = False
         while not not_the_same:
             wrong_two = random.choice(list_of_definitions)
@@ -101,6 +92,7 @@ def quiz():
                     continue
             else:
                 continue
+        #another loop also ensures that wrong_three is not the same as wrong_two or wrong_one
         not_the_same = False
         while not not_the_same:
             wrong_three = random.choice(list_of_definitions)
@@ -114,11 +106,18 @@ def quiz():
                     continue
             else:
                 continue
+        #list_of_options is created and shuffled to ensure that the responses are presented to the user in a random order
         list_of_options = [correct_definition, wrong_one, wrong_two, wrong_three]
         random.shuffle(list_of_options)
         return render_template("question.html", correct_word = correct_word, list_of_options = list_of_options)
     else: 
-        return "NOPE"         
+        #this means that the user HAS responded to a question (is not just accessing the page) and is NOT correct. 
+        #this means the user is incorrect
+        #directs user to a page that simply says "NOPE"
+        return "NOPE"      
+        
+           
+                 
 '''
 @app.route("/progress",methods=["GET"])
 def progress():
